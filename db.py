7b722a45cf367da6784dad75108096277bb5befa
FalkJark <73451426+FalkJark@users.noreply.github.com>
2021-02-07 12:15:50 -0600

    Get all data
    Get name, operation, values and i add bd.py to save information

---
+++ db.py
@@ -0,0 +1,11 @@
+rows = []
+
+class row:
+    def __init__(self,name,values,sort,search,valueSearch):
+        self.name = name
+        self.values = values
+        self.sort = sort
+        self.search = search
+        self.valueSearch = valueSearch
+
+    
--- read.py
+++ read.py
@@ -1,9 +1,53 @@
-def read():
-    string = ""
-    archivo = open("prueba.txt")
-    lines = archivo.readlines()
+import db
 
-    #while line !="":
-     #   line= archivo.readline() 
-    print(lines)
+def read():
+    nameFile = input("Ingresa la ruta de tu archivo: ")
+    archivo = open(nameFile)
+    lines = archivo.readlines()
+    archivo.close()
     
+    for i in lines:
+        i = i.rstrip("\n")
+    
+        #catch the name
+        tem = i.split("=")
+        nameRow = tem[0].rstrip(" ") # <----------- indica el nombre de la lista
+
+        #catch the operation sort
+        sortRow = False
+        if("ORDENAR" in tem[1]):
+            sortRow = True # <----------------- indica si hay que hacer ordenamiento
+            tem[1] = tem[1].replace("ORDENAR","")
+            tem[1] = tem[1].rstrip(" ")
+            tem[1] = tem[1].rstrip(",")
+
+        #catch the operation search
+        searchRow = False
+        if("BUSCAR" in tem[1]):
+            searchRow = True # <--------------- indica si hay que realizar una busqueda
+            tem[1] = tem[1].replace("BUSCAR","")
+            l = tem[1].rstrip(" ")
+            l = (len(tem[1]))
+            valueSearchRow = tem[1][l-1] # <---------- indica que valor debemos buscar
+            tem[1] = tem[1].rstrip(str(valueSearchRow))
+            tem[1] = tem[1].rstrip(" ")
+            tem[1] = tem[1].rstrip(",")
+
+        #catch the values
+        valuesTemp = tem[1].split(",")
+        valuesRow = []   # <---------- lista con los valores
+        for j in valuesTemp:
+            aux = int(j.strip())
+            valuesRow.append(aux)
+        
+        '''
+        #segmento de prueba
+        print("nombre de la lista: ",nameRow)
+        print("ordenar: ",sortRow)
+        print("buscar: ",searchRow)
+        if searchRow==True: 
+            print("Numero a buscar: ",valueSearchRow)
+        print("valore de la lista: ",valuesRow)
+        print("\n")
+
+        '''
